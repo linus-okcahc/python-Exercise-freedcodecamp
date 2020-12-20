0b5179d74a7d2f15273f5a357dac14cdf127a85b
Sunil Chacko <sunilchacko@hotmail.com>
2020-12-20 14:16:54 +0100

    add strings.py and variables.py

---
+++ strings.py
@@ -0,0 +1,12 @@
+phrase = "Girrafe Accademy"
+
+print(phrase)
+print (phrase + " is cool.")
+print("Giraffe\nAccademy")
+print("Giraffe \"Accademy\"")
+print(phrase.isupper())
+print(phrase.upper().isupper())
+print(len(phrase))
+print(phrase[0])
+print(phrase.index("a"))
+print(phrase.replace("Girrafe", "Elephant"))
---
+++ variables.py
@@ -0,0 +1,10 @@
+character_name = "John"
+character_age = "80"
+is_Male = True
+
+print("There once was a man named " + character_name + ", ")
+print("he was " + character_age + " years old. ")
+
+character_name = "Mike"
+print("He really liked the name " + character_name + ", ")
+print("but didn't like being "  + character_age + ". ")
