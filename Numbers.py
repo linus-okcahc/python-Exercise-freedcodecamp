61ff9ff49a60aff236e43718b943a0939afe1b07
Sunil Chacko <sunilchacko@hotmail.com>
2020-12-22 03:46:12 +0100

    update numbers.py

--- Numbers.py
+++ Numbers.py
@@ -1,4 +1,12 @@
+from math import *
+
 print(2.0987)
 print (3+4.5)
 my_num =5
 print(str(my_num) + " my fav number")
+my_second_num = -5
+print(abs(my_second_num))
+print(pow(my_second_num,2))
+print(floor(3.7))
+print(ceil(3.5))
+print(sqrt(36))
