26da28bf91b1b682e4c86295a0fefed8272e9d7b
Sunil Chacko <sunilchacko@hotmail.com>
2021-01-17 19:59:35 +0100

    add new files related to sequences

---
+++ sequenceOperation.py
@@ -0,0 +1,10 @@
+greeting = "Hello world"
+print(greeting[0])
+print(greeting[-1])
+
+#another way of indexing in sequences
+print('Hello'[0])
+
+#We can index the input from the user if its a sequence
+year = input('Year : ')[3]
+print(year)
